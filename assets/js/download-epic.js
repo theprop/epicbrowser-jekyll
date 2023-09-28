42ce21cd8baf7d3c72fc661136fad5bee5b5c8c7
Davy Kamanzi <davykamanzi@gmail.com>
2023-09-29 01:25:08 +0300

    Expanding Mac OS logic

--- assets/js/download-epic.js
+++ assets/js/download-epic.js
@@ -4,36 +4,55 @@ function detectOS() {
 
 	if (/android/i.test(userAgent)) return "Android";
 	if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) return "iOS";
-    if (/linux/i.test(userAgent)) return "Linux";
-	if (/mac/i.test(userAgent)) return "Mac OS";
+	if (/linux/i.test(userAgent)) return "Linux";
+	if (/mac/i.test(userAgent)) {
+		if (/intel/i.test(userAgent)) return "Mac OS (Intel-based)";
+		if (/arm/i.test(userAgent)) return "Mac OS (Arm-based)";
+	}
 	if (/win/i.test(userAgent)) return "Windows";
 	if (/windows phone/i.test(userAgent)) return "Windows Phone";
-    
+
 	return "Unknown";
 }
 
 // Function to trigger the download based on the detected OS
 function downloadFile() {
-	const userOS = detectOS();
-	let downloadLink = "";
+	try {
+		const userOS = detectOS();
+		let downloadLink = "";
 
-	switch (userOS) {
-		case "Windows":
-			downloadLink = "https://cdn.epicbrowser.com/winsetup/EpicSetup.exe";
-			break;
-		case "Mac OS":
-			downloadLink = "https://cdn.epicbrowser.com/macarm117/epic_117.0.5938.88.dmg";
-			break;
-		default:
-			alert("Sorry, we couldn't detect your operating system.");
-			return;
+		switch (userOS) {
+			case "Windows":
+				downloadLink = "https://cdn.epicbrowser.com/winsetup/EpicSetup.exe";
+				break;
+			case "Mac OS (Intel-based)":
+				downloadLink = "https://cdn.epicbrowser.com/macintel117/epic_117.0.5938.88.dmg";
+				break;
+			case "Mac OS (Arm-based)":
+				downloadLink = "https://cdn.epicbrowser.com/macarm117/epic_117.0.5938.88.dmg";
+				break;
+			case "Android":
+				downloadLink = "https://play.google.com/store/apps/details?id=com.epic.browser&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1";
+				break;
+			case "iOS":
+				downloadLink = "https://apps.apple.com/us/app/epic-privacy-browser-w-vpn/id1502032811?mt=8";
+				break;
+			default:
+				alert("Sorry, Epic is not yet available for your operating system.");
+				return;
+		}
+
+		// Create and trigger a download link
+		const link = document.createElement("a");
+		link.href = downloadLink;
+		if (userOS === "Windows" || userOS === "Mac OS (Intel-based)" || userOS === "Mac OS (Arm-based)") link.download = true;
+		link.click();
+	} catch (err) {
+		// Log error to console and alert user
+		console.error(err);
+		alert("Sorry, an error occurred.");
+		return;
 	}
-
-	// Create and trigger a download link
-	const link = document.createElement("a");
-	link.href = downloadLink;
-	link.download = true;
-	link.click();
 }
 
 // Call function on DOMContentLoaded
